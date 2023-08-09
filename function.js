25c2f7d0f51b5bf12b65bc25b25f687465278615
smartlycoding <joinalzx@gmail.com>
2023-08-09 19:47:15 +0600

    function practice

---
+++ function.js
@@ -0,0 +1,8 @@
+function myGrandTotal(num1, num2) {
+    const singleNumber = num1 + num2;
+    return singleNumber;
+}
+var giveNumber1 = 150;
+var giveNumber2 = 50;
+var totalNumber = myGrandTotal(giveNumber1, giveNumber2);
+console.log(totalNumber)
